67a4d9e52e277d903baeb69982580d9f1c1374b1
William Train <William.Train@ametek.com>
2025-02-10 21:16:44 +0000

    Creating project for first day

---
+++ template/src/main.rs
@@ -0,0 +1,19 @@
+#[allow(unused_imports)]
+use std::{fs, collections::HashMap, cmp::min, cmp::max};
+// use fancy_regex::Regex;
+// use regex::Regex;
+// use md5::{Digest, Md5};
+// use priority_queue::PriorityQueue;
+
+fn main() {
+    let input = fs::read_to_string("./inputs/puzzle_input.txt").expect("Failed to read input");
+    // let input = fs::read_to_string("./inputs/test_puzzle_input.txt").expect("Failed to read test input");
+    println!("{:?}", input);
+    println!("Input lenght: {}", input.len());
+
+    #[allow(unused_variables)]
+    for line in input.split("\n") {
+
+    }
+}
+
