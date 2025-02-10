06e1d7e5c8d183899a1cf670f6bd767bd9b496ae
William Train <William.Train@ametek.com>
2025-02-10 21:38:54 +0000

    Completed day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -11,9 +11,48 @@ fn main() {
     println!("{:?}", input);
     println!("Input lenght: {}", input.len());
 
-    #[allow(unused_variables)]
-    for line in input.split("\n") {
-
-    }
+    //part_one(&input);
+    part_two(&input);
 }
 
+#[allow(dead_code)]
+fn part_one(input: &str) {
+    let mut prev_int = 0;
+    let mut cur_int = 0;
+    let mut sum = 0;
+    for c in input.chars() {
+        prev_int = cur_int;
+        cur_int = c.to_digit(10).unwrap();
+        if prev_int == cur_int {
+            sum += cur_int;
+        }
+    }
+
+    if input.chars().next().unwrap().to_digit(10).unwrap() == cur_int {
+        sum += cur_int;
+    }
+
+    println!("Sum: {}", sum);
+}
+
+#[allow(dead_code)]
+fn part_two(input: &str) {
+    let mut sum = 0;
+    let cur_int = 0;
+    let comp_int = input.len() / 2;
+    let chars = input.chars().collect::<Vec<char>>();
+
+    for i in 0..input.len() {
+        let cur_int = chars[i].to_digit(10).unwrap();
+        let mut comp_index = i + comp_int;
+        if comp_index >= input.len() {
+            comp_index -= input.len();
+        } 
+        let comp_int = chars[comp_index].to_digit(10).unwrap();
+        if cur_int == comp_int {
+            sum += cur_int;
+        }
+    }
+
+    println!("Sum: {}", sum);
+}
