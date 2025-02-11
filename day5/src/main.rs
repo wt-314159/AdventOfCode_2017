85bc49604b4b5d5548eecd090116a18a4ac7b13e
William Train <William.Train@ametek.com>
2025-02-11 13:23:44 +0000

    Completing day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -10,19 +10,67 @@ fn main() {
     // let input = fs::read_to_string("./inputs/test_puzzle_input.txt").expect("Failed to read test input");
     println!("{:?}", input);
     println!("Input lenght: {}", input.len());
-
-    #[allow(unused_variables)]
-    for line in input.split("\n") {
-
-    }
+    
+    part_two(&input);
 }
 
 #[allow(dead_code)]
 fn part_one(input: &str) {
+    let mut instructions: Vec<i32> = input.split("\n")
+        .map(|s| {
+            if let Ok(num) = s.trim().parse::<i32>() {
+                return num;
+            }
+            else { 
+                println!("Failed to parse instruction: {}", s); 
+                42
+            }
+        })
+        .collect();
 
+    let mut idx: i32 = 0;
+    let mut steps = 0;
+    let len = instructions.len() as i32;
+    while idx >= 0 && idx < len {
+        let index = idx as usize;
+        let jump = instructions[index];
+        instructions[index] += 1;
+        idx += jump;
+        steps += 1;
+    }
+    println!("Steps: {}", steps);
 }
 
 #[allow(dead_code)]
 fn part_two(input: &str) {
-    
+    let mut instructions: Vec<i32> = input.split("\n")
+        .map(|s| {
+            if let Ok(num) = s.trim().parse::<i32>() {
+                return num;
+            }
+            else { 
+                println!("Failed to parse instruction: {}", s); 
+                42
+            }
+        })
+        .collect();
+
+    let mut idx: i32 = 0;
+    let mut steps = 0;
+    let len = instructions.len() as i32;
+
+    while idx >= 0 && idx < len {
+        let index = idx as usize;
+        let jump = instructions[index];
+        if jump >= 3 {
+            instructions[index] -= 1;
+        }
+        else {
+            instructions[index] += 1;
+        }
+        idx += jump;
+        steps += 1;
+    }
+    println!("Steps: {}", steps);
+ 
 }
