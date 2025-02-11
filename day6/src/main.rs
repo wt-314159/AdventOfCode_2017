47277171c6bd0588ca562c3638e1e5b8258414d8
William Train <William.Train@ametek.com>
2025-02-11 13:53:23 +0000

    completed day6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -11,18 +11,70 @@ fn main() {
     println!("{:?}", input);
     println!("Input lenght: {}", input.len());
 
-    #[allow(unused_variables)]
-    for line in input.split("\n") {
-
-    }
+    part_two(&input);
 }
 
 #[allow(dead_code)]
 fn part_one(input: &str) {
+    let mut past_iterations: Vec<[i32;16]> = Vec::new();
+    let mut banks = create_banks_array(input);
+    let mut count = 0;
 
+    while !past_iterations.contains(&banks) {
+        past_iterations.push(banks);
+        let max = find_max(&banks);
+        banks = *redistribute(&mut banks, max);
+        count += 1;
+    }
+    println!("No. cycles: {}", count);
 }
 
 #[allow(dead_code)]
 fn part_two(input: &str) {
-    
+    let mut past_iterations: HashMap<[i32;16], usize> = HashMap::new();
+    let mut banks = create_banks_array(input);
+    let mut count = 0;
+
+    while !past_iterations.contains_key(&banks) {
+        past_iterations.insert(banks, count);
+        let max = find_max(&banks);
+        banks = *redistribute(&mut banks, max);
+        count += 1;
+    }
+    println!("No. cycles: {}", count);
+    println!("Loop size: {}", count - past_iterations.get(&banks).unwrap());
+}
+
+fn create_banks_array(input: &str) -> [i32; 16] {
+    let mut banks = input.split_whitespace()
+        .map(|s| s.trim().parse::<i32>().unwrap());
+    let mut bank_array = [0; 16];
+    for i in 0..16 {
+        bank_array[i] = banks.next().unwrap();
+    }
+    bank_array
+}
+
+fn find_max(banks: &[i32; 16]) -> usize {
+    let mut max = 0;
+    for i in 0..16 {
+        if banks[i] > banks[max] {
+            max = i;
+        }
+    }
+    max
+}
+
+fn redistribute(banks: &mut [i32; 16], mut max: usize) -> &mut [i32; 16] {
+        let count = banks[max];
+        banks[max] = 0;
+
+        for _ in 1..count+1 {
+            max += 1;
+            if max >= 16 {
+                max = 0;
+            }
+            banks[max] += 1;
+        }
+        banks
 }
